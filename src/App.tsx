17b5a7275447a4952c492da2fd1d2fad00b16cbf
Ivan Klochkov <vany-nov13@yandex.ru>
2024-07-07 12:38:40 +0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <>test</>;
+import { Component } from 'react';
+import Header from './components/header/Header.tsx';
+
+class App extends Component {
+  handleSearch = (searchLine: string): void => {
+    alert(searchLine);
+  };
+
+  render() {
+    return (
+      <div>
+        <Header onSearch={this.handleSearch} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import logo from '../../../public/logo-movie.svg';
+
+interface SearchBarProps {
+  onSearch: (searchLine: string) => void;
+}
+
+interface SearchBarState {
+  searchLine: string;
+}
+
+class Header extends Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      searchLine: '',
+    };
+  }
+
+  handleSearch = (): void => {
+    const trimStr: string = this.state.searchLine.trim();
+    localStorage.setItem('luc', trimStr);
+    this.props.onSearch(trimStr);
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ searchLine: event.target.value });
+  };
+
+  handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      this.handleSearch();
+    }
+  };
+  render() {
+    return (
+      <header className="header">
+        <div className="header__wrapper">
+          <img src={logo} className="header__logo" alt="logo" />
+          <div className="header__search-str">
+            <input
+              type="text"
+              placeholder="Search"
+              value={this.state.searchLine}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyPress}
+            />
+            <button className="header__btn" onClick={this.handleSearch}>
+              Go
+            </button>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
