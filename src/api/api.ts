2d0d0c6ae6e5dc235dbefb41e2f02791b217ff40
Ivan Klochkov <vany-nov13@yandex.ru>
2024-07-08 18:50:31 +0300

    feat: update api interface

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,21 @@
 export interface CharacterBase {
-  character_name: string;
-  character_birth_year: string;
-  character_homeworld: string;
-  character_gender: string;
-  character_height: string;
-  character_mass: string;
-  character_eye_color: string;
-  character_skin_color: string;
-  character_hair_color: string;
-  character_url: string;
+  name: string;
+  birth_year: string;
+  homeworld: string;
+  gender: string;
+  height: string;
+  mass: string;
+  eye_color: string;
+  skin_color: string;
+  hair_color: string;
+  url: string;
 }
 
 export interface StarWarsResponse {
   results: CharacterBase[];
 }
 
-async function fetchData(searchStr: string, perPage: number): Promise<StarWarsResponse | undefined> {
+async function fetchData(searchStr: string, perPage: number): Promise<StarWarsResponse> {
   const response = await fetch(`https://swapi.dev/api/people/?search=${searchStr}&page=1&limit=${perPage}`);
   const data = await response.json();
   return data;
