3a116b148923b355cbaed34ebb9533505c33a076
Ivan Klochkov <vany-nov13@yandex.ru>
2024-07-13 23:00:57 +0300

    refactor: move types from api in types folder

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,6 @@
-export interface CharacterBase {
-  name: string;
-  birth_year: string;
-  homeworld: string;
-  gender: string;
-  height: string;
-  mass: string;
-  eye_color: string;
-  skin_color: string;
-  hair_color: string;
-  url: string;
-}
+import { StarWarsResponse } from '../types/character.interface.ts';
 
-export interface StarWarsResponse {
-  results: CharacterBase[];
-}
-
-async function fetchData(searchStr: string, perPage: number = 1): Promise<StarWarsResponse> {
+export async function fetchData(searchStr: string, perPage: number = 1): Promise<StarWarsResponse> {
   const response = await fetch(`https://swapi.dev/api/people/?search=${searchStr}&page=1&limit=${perPage}`);
   try {
     if (!response.ok) {
@@ -27,5 +12,3 @@ async function fetchData(searchStr: string, perPage: number = 1): Promise<StarWa
     throw new Error('Error');
   }
 }
-
-export { fetchData };
